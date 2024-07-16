9e40bc4b8d59da604ca6145767d78b826ab4dcc5
Kaim Arvas <skaimarvas@gmail.com>
2024-07-16 16:45:37 +0300

    add cors config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import * as cors from 'cors';
+
+dotenv.config({ path: '../.env' });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,PUT,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+  app.use(cors());
+  await app.listen(process.env.PORT);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
